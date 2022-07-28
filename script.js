91a02b0ef628d70320176b8effca1953d586de3b
Mark Struckmann <mark.struckmann@yahoo.com>
2022-07-28 17:24:34 -0500

    changed let to const

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Write your JavaScript code here!
-
-
 window.addEventListener("load", function() {
     
     const document = window.document
@@ -38,14 +35,14 @@ window.addEventListener("load", function() {
     }) 
     
     let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   
     let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
-        // Pick a planet from the list of planets and add that information to your destination.
+     
         let pickedPlanet = pickPlanet(listedPlanets);
 
         addDestinationInfo(window.document, pickedPlanet.name, pickedPlanet.diameter, pickedPlanet.star, pickedPlanet.distance, pickedPlanet.moons, pickedPlanet.image);
